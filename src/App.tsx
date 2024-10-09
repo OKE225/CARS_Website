33defd59484353486e15193b5c60618f6816d609
OKE225 <patryk.wroblewski.2006@gmail.com>
2024-10-09 18:24:52 +0200

    Add component - Arrows

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Navigation from "./components/Navigation";
-
+import Arrows from "./components/Arrows";
 import "./scss/App.scss";
 
 function App() {
   return (
     <div className="app">
       <Navigation />
+      <Arrows />
     </div>
   );
 }
