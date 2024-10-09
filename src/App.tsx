e04b9555c4797c0e1b03a21d81daea8650ffe8e6
OKE225 <patryk.wroblewski.2006@gmail.com>
2024-10-09 17:51:42 +0200

    Add component - Navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
+import Navigation from "./components/Navigation";
+
 import "./scss/App.scss";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Navigation />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Navigation extends Component {
+  render() {
+    return (
+      <nav id="navigation">
+        <h1>
+          CA<span className="color">RS</span>
+        </h1>
+      </nav>
+    );
+  }
+}
+
+export default Navigation;
