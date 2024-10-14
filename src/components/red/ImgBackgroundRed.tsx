92c8e04e743d4a66c6cd215fae90fb392f853f0f
OKE225 <patryk.wroblewski.2006@gmail.com>
2024-10-14 22:48:13 +0200

    Create component - ImgBackgroundRed

---
+++ src/components/red/ImgBackgroundRed.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import ferrariHome from "../../assets/img/ferrari-home.jpg";
+
+export default class ImgBackgroundRed extends Component {
+  render() {
+    return (
+      <div id="img-background-red">
+        <img src={ferrariHome} alt="" />
+      </div>
+    );
+  }
+}
