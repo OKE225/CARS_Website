fd0d0cad43212fcdb28a3526f5e223b0038106c1
OKE225 <patryk.wroblewski.2006@gmail.com>
2024-10-14 22:42:34 +0200

    Create component - NavigationRed

---
+++ src/components/red/NavigationRed.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class NavigationRed extends Component {
+  render() {
+    return (
+      <nav id="navigation-red">
+        <h1>
+          CA<span>RS</span>
+        </h1>
+      </nav>
+    );
+  }
+}
