e27526ed307952114f65d82272c0742bbc387639
OKE225 <patryk.wroblewski.2006@gmail.com>
2024-10-14 22:48:36 +0200

    Import components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
 
 import "./scss/App.scss";
+import NavigationRed from "./components/red/NavigationRed";
+import Arrows from "./components/Arrows";
+import CardHomeRed from "./components/red/CardHomeRed";
+import ImgBackgroundRed from "./components/red/ImgBackgroundRed";
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-        <h1>Hello World</h1>
+        <ImgBackgroundRed />
+        <NavigationRed />
+        <CardHomeRed />
+        <Arrows />
       </div>
     );
   }
