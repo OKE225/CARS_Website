04c9abd6143d8f40d1a496c2846f1d163a59b18c
OKE225 <patryk.wroblewski.2006@gmail.com>
2024-10-15 22:43:15 +0200

    Import CarGreen component & related components

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import React, { Component } from "react";
 
 import "./scss/App.scss";
 import Arrows from "./components/Arrows";
-import CarRed from "./components/red/CarRed";
+// import CarRed from "./components/red/CarRed";
+import CarGreen from "./components/green/CarGreen";
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-        <CarRed />
+        {/* <CarRed /> */}
+        <CarGreen />
         <Arrows />
       </div>
     );
