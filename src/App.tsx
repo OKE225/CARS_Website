b4089a36fcb2c15527d92579c49097ba31319476
OKE225 <patryk.wroblewski.2006@gmail.com>
2024-10-15 17:15:30 +0200

    Create component - CarRed

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
 import React, { Component } from "react";
 
 import "./scss/App.scss";
-import NavigationRed from "./components/red/NavigationRed";
 import Arrows from "./components/Arrows";
-import CardHomeRed from "./components/red/CardHomeRed";
-import ImgBackgroundRed from "./components/red/ImgBackgroundRed";
+import CarRed from "./components/red/CarRed";
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-        <ImgBackgroundRed />
-        <NavigationRed />
-        <CardHomeRed />
+        <CarRed />
         <Arrows />
       </div>
     );
---
+++ src/components/red/CarRed.tsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import ImgBackgroundRed from "./ImgBackgroundRed";
+import NavigationRed from "./NavigationRed";
+import CardHomeRed from "./CardHomeRed";
+
+export default class CarRed extends Component {
+  render() {
+    return (
+      <>
+        <ImgBackgroundRed />
+        <NavigationRed />
+        <CardHomeRed />
+      </>
+    );
+  }
+}
