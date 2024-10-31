0f91b1e84276b1ef5f3ec484485d2eb96e82c763
OKE225 <patryk.wroblewski.2006@gmail.com>
2024-10-31 23:06:07 +0100

    Add react router to heading navigation

--- src/components/green/NavigationGreen.tsx
+++ src/components/green/NavigationGreen.tsx
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export default class NavigationGreen extends Component {
   render() {
     return (
       <nav id="navigation-green">
-        <h1>
-          CA<span>RS</span>
-        </h1>
+        <Link to="/">
+          <h1>
+            CA<span>RS</span>
+          </h1>
+        </Link>
       </nav>
     );
   }
