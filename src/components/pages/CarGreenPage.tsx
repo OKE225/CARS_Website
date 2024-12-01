4ebccc9fc0b7054738dc1a7a973978d3f13f33f7
OKE225 <patryk.wroblewski.2006@gmail.com>
2024-12-02 00:07:26 +0100

    Import components

--- src/components/pages/CarGreenPage.tsx
+++ src/components/pages/CarGreenPage.tsx
@@ -4,19 +4,27 @@ import MainContainer from "../green/page/MainContainer";
 import SectionRightText from "../green/page/SectionRightText";
 import SectionLeftText from "../green/page/SectionLeftText";
 import SectionWithMobileImg from "../green/page/SectionWithMobileImg";
+import PrevBtn from "../green/page/changePage/PrevBtn";
+import NextBtn from "../green/page/changePage/NextBtn";
+import Footer from "../green/page/footer/Footer";
 
 export default class CarGreenPage extends Component {
   render() {
     return (
-      <>
+      <div className="green-page">
         <NavigationGreen />
         <MainContainer />
         <div className="content">
           <SectionRightText />
           <SectionLeftText />
           <SectionWithMobileImg />
+          <div className="btn-group">
+            <PrevBtn />
+            <NextBtn />
+          </div>
         </div>
-      </>
+        <Footer />
+      </div>
     );
   }
 }
--- src/components/pages/CarVioletPage.tsx
+++ src/components/pages/CarVioletPage.tsx
@@ -4,19 +4,27 @@ import MainContainer from "../violet/page/MainContainer";
 import SectionRightText from "../violet/page/SectionRightText";
 import SectionLeftText from "../violet/page/SectionLeftText";
 import SectionWithMobileImg from "../violet/page/SectionWithMobileImg";
+import PrevBtn from "../violet/page/changePage/PrevBtn";
+import NextBtn from "../violet/page/changePage/NextBtn";
+import Footer from "../violet/page/footer/Footer";
 
 export default class CarVioletPage extends Component {
   render() {
     return (
-      <>
+      <div className="violet-page">
         <NavigationViolet />
         <MainContainer />
         <div className="content">
           <SectionRightText />
           <SectionLeftText />
           <SectionWithMobileImg />
+          <div className="btn-group">
+            <PrevBtn />
+            <NextBtn />
+          </div>
         </div>
-      </>
+        <Footer />
+      </div>
     );
   }
 }
