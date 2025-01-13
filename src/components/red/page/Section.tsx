e343d6794bae5e00d61bc283dac521fc86285cc3
OKE225 <patryk.wroblewski.2006@gmail.com>
2025-01-13 07:57:50 +0100

    Create Section component and replace existing usage

---
+++ src/components/red/page/Section.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import ImgTextSection from "./ImgTextSection";
+import ImgTextSection2 from "./ImgTextSection2";
+import MobileImgTextSection from "./MobileImgTextSection";
+
+type Props = {
+  type: "right-text" | "left-text" | "mobile";
+  header: string;
+  paragraph1: string;
+  paragraph2: string;
+  img?: string;
+};
+
+const Section: React.FC<Props> = ({
+  type,
+  header,
+  paragraph1,
+  paragraph2,
+  img,
+}) => {
+  switch (type) {
+    case "right-text":
+      return (
+        <div id="section-right-text-red">
+          <ImgTextSection />
+          <div className="text-container">
+            <h2>{header}</h2>
+            <p>{paragraph1}</p>
+            <p>{paragraph2}</p>
+          </div>
+        </div>
+      );
+    case "left-text":
+      return (
+        <div id="section-left-text-red">
+          <div className="text-container">
+            <h2>{header}</h2>
+            <p>{paragraph1}</p>
+            <p>{paragraph2}</p>
+          </div>
+          <ImgTextSection2 />
+        </div>
+      );
+    case "mobile":
+      return (
+        <div id="section-mobile-text-red">
+          <MobileImgTextSection />
+          <div className="text-container">
+            <h2>{header}</h2>
+            <p>{paragraph1}</p>
+            <p>{paragraph2}</p>
+          </div>
+        </div>
+      );
+  }
+  return null;
+};
+
+export default Section;
